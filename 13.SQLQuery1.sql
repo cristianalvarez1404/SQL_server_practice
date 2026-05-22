f131d4054d8818e8a9b414a773cf77530d00821d
cristian <cristianalvarez1496@gmail.com>
2026-05-21 21:40:25 -0500

    windows function - Over

--- 13.SQLQuery1.sql
+++ 13.SQLQuery1.sql
@@ -36,6 +36,82 @@ SELECT
 	SUM(Sales) OVER(PARTITION BY ProductID) AS total_sales_by_product
 FROM Sales.Orders;
 
+-- Find the total sales across all orders
+-- Additionally provide details such order ID, order date
+
+SELECT
+OrderID,
+OrderDate,
+SUM(Sales) OVER() AS TotalSales
+FROM Sales.Orders;
+
+-- Find the total sales for each product, additionally privide
+-- details such order id & order date
+
+SELECT
+	OrderID,
+	OrderDate,
+	ProductId,
+	SUM(Sales) OVER(PARTITION BY productID) AS TotalSales
+FROM Sales.Orders;
+
+-- Find the total sales across all orders
+-- Find the total sales for each product
+-- Additionally provide details such order Id, order date
+
+SELECT
+	OrderID,
+	OrderDate,
+	ProductID,
+	Sales,
+	SUM(Sales) OVER() TotalSales,
+	SUM(Sales) OVER (PARTITION BY ProductID) TotalSalesByProducts
+FROM Sales.Orders;
+
+-- Find the total sales across all orders
+-- Find the total sales for each product
+-- Find the total sales for each combination of product and order status
+-- Additionally provide details such order Id, order date
+
+SELECT
+	OrderID,
+	OrderDate,
+	Sales,
+	ProductID,
+	OrderStatus,
+	SUM(Sales) OVER() TotalSales,
+	SUM(Sales) OVER(PARTITION BY ProductID) SalesByProducts,
+	SUM(Sales) OVER(PARTITION BY ProductID,OrderStatus) SalesByProductsAndStatus
+FROM Sales.Orders; 
+
+
+SELECT
+*
+FROM Sales.Orders;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
