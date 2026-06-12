993d90c7ca88c477cbef5157aa794b25bf00a77f
CristianAlvarez <cristianandres1496@gmail.com>
2026-06-11 21:51:27 -0500

    ntile

--- 14.SQLQuery1.sql
+++ 14.SQLQuery1.sql
@@ -22,4 +22,80 @@ FROM
 FROM Sales.Orders
 ) t WHERE SalesRank = 1;
 
+-- Find the lowest 2 customers based on their total sales
+
+SELECT *
+FROM (
+SELECT
+	CustomerID,
+	SUM(Sales) TotalSales,
+	ROW_NUMBER() OVER(ORDER BY SUM(Sales)) RankCustomers
+FROM Sales.Orders
+GROUP BY CustomerID
+) t WHERE RankCustomers <= 2;
+
+-- Asign unique IDs to the rows of the 'Orders Archive'
+SELECT
+ROW_NUMBER() OVER(ORDER BY OrderID, OrderDate) UniqueID,
+*
+FROM Sales.OrdersArchive;
+
+-- Identify duplicate rows in the table 'Orders Archive' 
+-- and return a clean result without any duplicates
+
+SELECT
+*
+FROM (
+SELECT
+ROW_NUMBER() OVER(PARTITION BY OrderID ORDER BY CreationTime DESC) AS rn ,
+*
+FROM Sales.OrdersArchive
+) t WHERE rn > 1;
+
+
+SELECT
+	OrderID,
+	Sales,
+	NTILE(2) OVER(ORDER BY Sales DESC) TwoBucket,
+	NTILE(3) OVER(ORDER BY Sales DESC) ThreeBucket
+FROM Sales.Orders
+
+-- Segment all orders into 3 categories: high, medium and low sales.
+
+SELECT
+*,
+ CASE
+	WHEN Buckets = 1 THEN 'High'
+	WHEN Buckets = 2 THEN 'Medium'
+	WHEN Buckets = 3 THEN 'Low'
+ END AS SalesSegmentation
+FROM (
+SELECT
+	OrderID,
+	Sales,
+	NTILE(3) OVER(ORDER BY Sales DESC) Buckets
+FROM Sales.Orders
+) t 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
