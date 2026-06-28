310f7f4d5a96007c3fb343d0c57485c3e0cc1537
cristiann <cristianandres1496@gmail.com>
2026-06-27 21:53:29 -0500

    subqueries on join

--- 16.SQLQuery1.sql
+++ 16.SQLQuery1.sql
@@ -38,6 +38,69 @@ FROM
 	FROM Sales.Products
 ) t WHERE Price > AVGPrice
 
+-- Rank customers based on their total amount of sales
+
+SELECT
+	DENSE_RANK() OVER(ORDER BY total_sales DESC) AS rank_customers,
+	*
+FROM (
+SELECT
+	CustomerID,
+	Sales,
+	SUM(Sales) OVER(PARTITION BY CustomerID) AS total_sales
+FROM Sales.Orders
+) t ;
+
+SELECT
+*,
+RANK() OVER(ORDER BY TotalSales) CustomerRank
+FROM
+(
+SELECT
+	CustomerID,
+	SUM(Sales) TotalSales
+FROM Sales.Orders
+GROUP BY CustomerID) t;
+
+-- SELECT CLAUSE
+
+-- Show the product IDs, names, prices and total number of orders.
+
+SELECT 
+	ProductID,
+	Product,
+	Price,
+	(SELECT COUNT(*) FROM Sales.Orders) AS total_orders
+FROM Sales.Products;
+
+-- Join clause
+
+-- Show all customer details and find the total orders of each customer
+SELECT
+c.*,
+o.totalOrders
+FROM Sales.Customers AS c
+LEFT JOIN (
+SELECT
+	CustomerID,
+	COUNT(*) AS totalOrders
+	FROM Sales.Orders GROUP BY CustomerID
+) AS o 
+ON c.CustomerID = o.CustomerID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
