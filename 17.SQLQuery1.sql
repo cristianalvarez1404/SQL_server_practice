60ea0be32b178ff9e0a4024bdb1bcdb19351a0ad
cristiann <cristianandres1496@gmail.com>
2026-07-11 21:43:39 -0500

    CTA recursive

--- 17.SQLQuery1.sql
+++ 17.SQLQuery1.sql
@@ -109,6 +109,58 @@ LEFT JOIN CTE_Customer_segments AS ccs
 ON ccs.CustomerID = c.CustomerID; 
 
 
+-- Recursive CTE
+
+-- Generate a Sequence of numbers from 1 to 20
+
+WITH Series AS (
+	-- Anchor Query
+	SELECT
+		1 AS MyNumber
+	UNION ALL
+	-- Recursive query
+	SELECT
+		MyNumber + 1
+	FROM Series
+	WHERE MyNumber < 20
+)
+-- Main Query
+SELECT *
+FROM Series
+OPTION (MAXRECURSION 10)
+
+-- Task: Show the employee hierarchy by displaying each employee's level within the organization
+WITH CTE_Emp_Hierarchy AS 
+(
+	-- Anchor query
+	SELECT
+		EmployeeID,
+		FirstName,
+		ManagerID,
+		1 AS Level
+	FROM Sales.Employees
+	WHERE ManagerID IS NULL
+	UNION ALL
+	-- Recursive Query
+	SELECT
+		e.EmployeeID,
+		e.FirstName,
+		e.ManagerID,
+		Level + 1
+	FROM Sales.Employees AS e
+	INNER JOIN CTE_Emp_Hierarchy AS ceh
+	ON e.ManagerID = ceh.EmployeeID
+)
+--Main Query
+SELECT
+*
+FROM CTE_Emp_Hierarchy
+
+
+
+
+
+
 
 
 
