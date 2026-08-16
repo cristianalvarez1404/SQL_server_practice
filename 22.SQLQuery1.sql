a261f0683ec64c6b7ab931c4c78b9c0de71fa240
cristiann <cristianandres1496@gmail.com>
2026-08-15 21:19:02 -0500

    unique and filter index

--- 22.SQLQuery1.sql
+++ 22.SQLQuery1.sql
@@ -34,3 +34,61 @@ COLUMNSTORE INDEX =>  -ORGANIZES AND STORES DATA COLUMN BY COLUMN
                       -FAST READ PERFORMANCE,SLOW WRITE PERFORMANCE
                       -HIGHER => RETRIVE SPECIFIC COLUMNS
 */
+
+CREATE CLUSTERED COLUMNSTORE INDEX idx_DBCustomers_CS ON Sales.DBCustomers
+
+DROP INDEX idx_DBCustomers_CS ON Sales.DBCustomers
+
+CREATE NONCLUSTERED COLUMNSTORE INDEX idx_DBCustomers_CS ON Sales.DBCustomers_CS_FirstName ON Sales.DBCustomers(FirstName)
+
+/*-----------------------------------------------------------*/
+
+USE AdventureWorksDW2022
+
+-- HEAP
+SELECT *
+INTO FactInternetSales_HP
+FROM FactInternetSales
+
+-- RowStore
+SELECT *
+INTO FactInternetSales_RS
+FROM FactInternetSales
+
+CREATE CLUSTERED INDEX idx_FactInternetSales_RS_PK
+ON FactInternetSales_RS (SalesOrderNumber, SalesOrderLineNumber)
+
+
+-- ColumnStore
+SELECT *
+INTO FactInternetSales_CS
+FROM FactInternetSales
+
+CREATE CLUSTERED COLUMNSTORE INDEX idx_FactInternetSales_CS_PK
+ON FactInternetSales_CS 
+
+/*--------------------------------------------------------------*/
+--UNIQUE INDEX => NOT DUPLICATES -> The table must be empty
+
+SELECT * FROM Sales.Products
+
+CREATE UNIQUE NONCLUSTERED INDEX idx_Products_Product
+ON Sales.Products(Product)
+
+INSERT INTO Sales.Products(ProductID, Product) VALUES (106, 'Caps')
+
+/*-----------------------------------------------------------------*/
+-- FILTER INDEX => WE JUST ADD A WHERE. IN WHERE WE SPECIFIED THE CONDITION.
+  -- We cannot create a filter index on a clustered index
+  -- We cannot create a filter index on a columstore index
+
+SELECT * FROM Sales.customers
+WHERE Country = 'USA'
+
+CREATE NONCLUSTERED INDEX idx_Customers_Country 
+ON Sales.Customers(Country)
+WHERE Country = 'USA' -- FILTER INDEX
+
+
+
+
