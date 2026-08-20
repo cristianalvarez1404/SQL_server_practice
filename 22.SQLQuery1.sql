7479945c841bdda636923104afb4caa2845af05a
cristiann <cristianandres1496@gmail.com>
2026-08-19 21:09:57 -0500

    index monitoring

--- 22.SQLQuery1.sql
+++ 22.SQLQuery1.sql
@@ -89,6 +89,56 @@ CREATE NONCLUSTERED INDEX idx_Customers_Country
 ON Sales.Customers(Country)
 WHERE Country = 'USA' -- FILTER INDEX
 
+/*-----------------------------------Manage Index----------------------*/
+-- List all indexes on a specific table
+sp_helpindex 'Sales.DBCutomers'
 
+-- Monitoring index usage => We use metadata 
+SELECT 
+  tbl.name AS TableName,
+  idx.name AS IndexName,
+  idx.type_desc AS IndexType,
+  idx.is_primary_key AS IsPrimaryKey,
+  idx.is_unique AS IsUnique,
+  idx.is_disabled AS IsDisabled
+  s.user_seeks AS UserSeeks,
+  s.user_scans AS UserScans,
+  s.user_lookups AS UserLookups,
+  s.user_updates AS UserUpdates,
+  COALESCE(s.last_user_seek, s.last_user_scan) AS LastUpdate
+FROM sys.indexes AS idx
+JOIN sys.tables AS tbl
+ON idx.object_id = tbl.object_id
+LEFT JOIN sys.dm_db_index_usage_stats AS s
+ON s.object_id = idx.object_id AND s.index_id = idx.index_id
+ORDER BY tbl.name, idx.name;
 
+SELECT * FROM sys.tables;
+SELECT * FROM sys.dm_db_index_usage_stats
 
+/*------------------------------------------------------------*/
+SELECT
+  fs.SalesOrderNumber,
+  dp.EnglishProductName,
+  dp.Color
+FROM FactInternetSales AS fs
+INNER JOIN DimProduct dp
+ON fs.ProductKey = dp.ProductKey
+WHERE dp.Color = 'Black'
+AND fs.OrderDateKey BETWEEN 20101229 AND 20101231
+
+SELECT * FROM sys.dm_db_missing_index_details
+
+-- Defragment
+SELECT * 
+FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, 'LIMITED')
+
+ALTER INDEX idx_Customers_CS_Country ON Sales.Customers REORGANIZE
+ALTER INDEX idx_Customers_Country ON Sales.Customers REBUILD
+
+/*-----------------Index managment-----------------------*/
+-- Monitor index usage
+-- Monitor missing indexes
+-- Monitor duplicate indexes
+-- Update statistics
+-- Monitor fragmentations
